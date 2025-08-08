b0f7672be38b131fb3157ec586b3e8042de21246
YashwantHabib <yashwanthabib567@gmail.com>
2025-08-08 17:57:53 +0530

    backendUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://clinic-backend-ik2b.onrender.com/",
 });
 
 API.interceptors.request.use((config) => {
